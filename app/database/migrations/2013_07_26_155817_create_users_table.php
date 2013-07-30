5393b673746195a3a994bc6a2f4447767092e0d3
aitiba <aitiba@riseup.net>
2013-07-30 20:58:40 +0200

    Los tests van a ser tests de aceptacion en vez de unitarios. Se hacen con codeception y acabo se configurar el entorno de testing para que cuando se ejecute un run de codeception tire de una base de datos SQLite. Mas info en http://burujabetech.net/wiki/wiki/usar-codeception-en-entorno-de-pruebas-con-larave4

--- app/database/migrations/2013_07_26_155817_create_users_table.php
+++ app/database/migrations/2013_07_26_155817_create_users_table.php
@@ -14,11 +14,14 @@ class CreateUsersTable extends Migration {
     {
         Schema::create('users', function(Blueprint $table) {
             $table->increments('id');
-            $table->integer('groud_id');
-			$table->string('name');
-			$table->string('email');
-			$table->string('password');
-			$table->string('username');
+            if (App::environment() == 'testing') {
+                $table->string('codeception_added_auto_submit');
+            }
+            $table->integer('group_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('username');
 			$table->datetime('deleted_at');
             $table->timestamps();
         });
--- app/views/users/edit.blade.php
+++ app/views/users/edit.blade.php
@@ -2,37 +2,40 @@
 
 @section('content')
 
-{{ $user->name }}
 
 <p>{{ Lang::get('messages.NEW USER') }} </p>
 {{ Form::open(array('url' => 'users')) }}
 
+{{ Form::token(); }}
 <!-- group_id -->
-{{ $errors->first('group_id', 'HOLA') }}
-{{ Form::select('group_id', array(1 => 'admin'), 'hola', array('required')) }}
+{{ Form::label('group_id',  Lang::get('users.group_id')); }}
+{{ $errors->first('group_id', '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>:message</div>') }}
+{{ Form::select('group_id', array(1 => 'admin'), $user->group_id, array('required')); }}
 
 <!-- name -->
-
-{{ $errors->first('name', 'aaaaa') }}
-{{ Form::text('name', 'name' ,array('required')) }}
+{{ Form::label('name', Lang::get('users.name')); }}
+{{ $errors->first('name', '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>:message</div>') }}
+{{ Form::text('name', $user->name ,array('required')); }}
 
 <!-- email -->
-
-{{ $errors->first('email', 'ggg') }}
-{{ Form::email('email', '$user->email', array('required')) }}
+{{ Form::label('email', Lang::get('users.email')); }}
+{{ $errors->first('email', '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>:message</div>') }}
+{{ Form::email('email', $user->email, array('required')); }}
 
 <!-- password -->
-
+{{ Form::label('password', Lang::get('users.password')); }}
 {{ $errors->first('password', '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>:message</div>') }}
-{{ Form::password('password', null ,array('required')) }}
+{{ Form::password('password', null ,array('required')); }}
 <p>Pasahitzak minimo zenbaki bat, letra xehe bat, lerra larri bat eta hauetako karaktere bereziren bat: @#$%!_$% behar du eduki eta 8 eta 20 karaktere artean.</p>
 
 <!-- username -->
-
+{{ Form::label('username', Lang::get('users.username')); }}
 {{ $errors->first('username', '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>:message</div>') }}
-{{ Form::text('username', '$user->username' ,array('required')) }}
+{{ Form::text('username', $user->username ,array('required')); }}
 
 <!-- submit -->
+{{ Form::submit( Lang::get('users.Send!')); }}
 
-
+{{ HTML::script('js/ckeditor.js') }}
+{{ HTML::script('js/sample.js') }}
 @stop
